5291f6c5191e49f258b9a9fcfcdb18fb05eeba6a
brad467388 <dream010436@gmail.com>
2024-03-20 15:05:49 +0800

    完成user註冊表單post回傳值的取得

--- user/views.py
+++ user/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 
 def user_register(request):
-    return render(request, "user/register.html")
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        username = request.POST.get('username')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        print("post", request.POST)
+        print(username, password1, password2)
+    return render(request, "user/register.html", {"form": form})
