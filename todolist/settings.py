7f733c93b1f2420b950bbe91a8c426ba4c920d16
brad467388 <dream010436@gmail.com>
2024-03-25 17:53:51 +0800

    初始化完成todo功能

--- todolist/settings.py
+++ todolist/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "user.apps.UserConfig"
+    "user.apps.UserConfig",
+    "todo.apps.TodoConfig"
 ]
 
 MIDDLEWARE = [
