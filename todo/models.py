23e6d81a32ee821d520ad7dcccb87928e745fd42
brad467388 <dream010436@gmail.com>
2024-03-26 12:40:40 +0800

    建立建立todo資料模型完成

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField(blank=True)
+    created = models.DateField(auto_now_add=True)
+    date_completed = models.DateTimeField(blank=True, null=True)
+    important = models.BooleanField(default=False)
+    # todo_id <=> user_id 外鍵關聯
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return F'[{self.created}] {self.title}-{self.user}'
